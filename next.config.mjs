17b33c0201198a9a533511c4539756956e964f4b
Sherzod Rajabov <sherzodradjabov0625@gmail.com>
2026-03-01 23:45:09 +0500

    Fix: Add remotePatterns to bypass Next Image domain restrictions

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        optimizePackageImports: ["@untitledui/icons"],
-    },
+  experimental: {
+    optimizePackageImports: ["@untitledui/icons"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
